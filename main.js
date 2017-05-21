525c294b6b282cfe4b2adf8af19559cbcd4ba47c
Lauren Wilson <emaillaurenwilson@gmail.com>
2017-05-21 12:45:05 +0100

    w/HandleBars
    Now using HandleBars framework to demonstrate Seperation of concerns

--- main.js
+++ main.js
@@ -1,48 +1,47 @@
-//OpenWeatherMap API 
+//Global Variables
 var OPEN_WEATHER_MAP_API = "https://circuits-api.generalassemb.ly/8737fcf3-6a39-4548-a324-209d535e59fd?q=";
-//global variable to store base of API URI
+var resultElement = $("#result");
 
-var resultElement = $('#result');
-// assign result element to variable
+function callOpenWeatherMap(city) {
 
-$('#searchBtn').on('click', function(e){
-  e.preventDefault();
-  var city = $('#searchField').val();
-  //get user input and assign it to var of city, which we then pass into the function callOpenWeatherMap
-  callOpenWeatherMap(city);
-});
-//search DOM for search button, add click event listener
+    if (city.length == 0) {
+      $("#result").html("Please enter a city name into the search field");
+    } else {
 
+        $.get(OPEN_WEATHER_MAP_API + city, function(searchResult) {
+          // convert Kelvin temperature to Celsius
+          var celsiusTemp = toCelsius(searchResult.main.temp);
+
+          // grab template HTML from DOM
+          var source = $('.weather-result-template').html();
+
+          // compile template
+          var template = Handlebars.compile(source);
+
+          // define weather data object that will be used in template
+          var weatherTemplateData = {
+          city: searchResult.name,
+          temp: celsiusTemp
+        };
+      // inject weather data into template
+      // and create template
+      var weatherResultHTML = template(weatherTemplateData);
+
+      resultElement.html(weatherResultHTML);
+
+      });
+
+    }
+
+}
 
 function toCelsius(kelvinTemp) {
   var temp = Math.round(kelvinTemp - 273.15);
   return temp;
 }
-//function to convert Kelvin temperature to Celsius
 
- function callOpenWeatherMap(city) {
-    if (city.length == 0) {
-      $("#result").html("Please enter a city name into the search field");
-    } 
-   //check to see that user has input a city name, if city name blank, present message back to result field to prompt entry
-   else {
-	  $.get( OPEN_WEATHER_MAP_API + city, function(searchResult){
-      console.log(searchResult.main.temp);
-      
-      var celsiusTemp = toCelsius(searchResult.main.temp);
-      //create var celsiusTemp, then pass the temperature result data into via function above, to celsius, converting kelvin temp to celsius value. celsiusTemp var holds converted output
-        
-      var stringOutput =  stringOutput = "<p>"+ searchResult.name + "<br>"; //adding result city name
-            stringOutput += "<p>"+ celsiusTemp + " C" + "<br>"; //adding result temperature
-            stringOutput +="</p>";
-      //variable to contain what we push to screen
-      
-      resultElement.html(stringOutput);
-      // use string searchResult to fill markup with ID resultElement on screen  
-      });
-    }
-//make a GET request to the API for the city given by user.
-//create anonymous success handler function which checks the reply by logging temp
-}
-//create funtction to call the API, passing city as arguement
+$("#searchBtn").on('click', function (event){
+    event.preventDefault(); 
 
+    callOpenWeatherMap($("#searchField").val());
+});
